d80372ea55345a053c9361b32bfdd2e691b4337b
Manoj Tarad <manojtarad65@gmail.com>
2025-12-04 21:49:07 +0530

    done

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,12 +6,16 @@ if (!uri) throw new Error("Missing MONGODB_URI");
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 if (process.env.NODE_ENV === "development") {
-  if (!(global as any)._mongoClientPromise) {
+  if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
-    (global as any)._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri);
   clientPromise = client.connect();
